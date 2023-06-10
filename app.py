79f5bbfccae45d9a9a848e6ecc3cf9f3525e0832
edwar3je <jameserikedwards@gmail.com>
2023-06-09 20:15:29 -0400

    modified app.py to use sessions to track variables (allows multiple people to use application at once) modified the action for the form on home.html so that it goes to a new route which will initialize a few variables as sessions.

--- app.py
+++ app.py
@@ -1,69 +1,92 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this-is-secret'
 
-responses = []
-reslen = len(responses)
-ind = -1
-indlen = (len(satisfaction_survey.questions)) - 1
+satlen = len(satisfaction_survey.questions)
+indlen = (satlen - 1)
 
 @app.route('/home')
 def home_display():
+    session['start'] = False
     return render_template('home.html', title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
 
 @app.route('/question/<ind>')
 def quest_display(ind):
-    path = request.path.replace('/question/', '')
-    num_path = int(path)
-    if not responses:
-        if num_path == 0:
-            return render_template('question.html', ind=ind, ind_n = int(ind), question=satisfaction_survey.questions[int(ind)].question, choices=satisfaction_survey.questions[int(ind)].choices)
-        elif num_path > indlen:
-            ind = 0
-            flash("That question doesn't exist.")
-            return redirect(str(ind))
-        else:
-            ind = 0
-            flash("No peeking.")
-            return redirect(str(ind))
-    else:
-        if num_path < len(responses):
-            ind = len(responses)
-            flash("Can't access previously answered questions.")
-            return redirect('/question/' + str(ind))
-        elif num_path > len(responses):
-            if num_path > indlen:
-                ind = len(responses)
-                flash("That question doesn't exist.")
-                return redirect('question/' + str(ind))
+    if session['start']:
+        session['numpath'] = int(request.path.replace('/question/', ''))
+        # if responses is empty
+        if not session['responses']:
+            if session['numpath'] == 0:
+                return render_template('question.html', ind=str(session['ind']), ind_n = int(session['ind']), question=satisfaction_survey.questions[int(session['ind'])].question, choices=satisfaction_survey.questions[int(session['ind'])].choices)
             else:
-                ind = len(responses)
-                flash("No peeking.")
-                return redirect('question/' + str(ind))
+                if session['numpath'] <= indlen:
+                    session['ind'] = 0
+                    flash('No skipping ahead')
+                    return redirect(str(session['ind']))
+                else:
+                    session['ind'] = 0
+                    flash("That question doesn't exist")
+                    return redirect(str(session['ind']))
+        # if responses is not entirely full
+        elif len(session['responses']) < satlen:
+            if session['numpath'] <= indlen:
+                if session['numpath'] == session['ind']:
+                    return render_template('question.html', ind=str(session['ind']), ind_n = int(session['ind']), question=satisfaction_survey.questions[int(session['ind'])].question, choices=satisfaction_survey.questions[int(session['ind'])].choices)
+                elif session['numpath'] < session['ind']:
+                    session['ind'] = len(session['responses'])
+                    flash("No turning back")
+                    return redirect(str(session['ind']))
+                else:
+                    session['ind'] = len(session['responses'])
+                    flash("No skipping ahead")
+                    return redirect(str(session['ind']))
+            else:
+                session['ind'] = len(session['responses'])
+                flash("That question doesn't exist")
+                return redirect(str(session['ind']))
+        # if responses is either full or overflowing
         else:
-            return render_template('question.html', ind=ind, ind_n = int(ind), question=satisfaction_survey.questions[int(ind)].question, choices=satisfaction_survey.questions[int(ind)].choices)
+            return redirect('/thanks')
+    else:
+        flash('Please start the survey')
+        return redirect('/home')
 
-@app.route('/question/<ran>')
+@app.route('/handle-process')
+def init_sessions():
+    session['start'] = True
+    session['ind'] = 0
+    session['responses'] = []
+    print(session['ind'])
+    print(session['responses'])
+    return redirect('/question/' + str(session['ind']))
 
 @app.route('/thanks', methods=['GET', 'POST'])
-def display_answer():
-    global ind
-    if ind != indlen:
-        if ind == -1:
-            ind += 1
-            return redirect('/question/' + str(ind))
-        else:
-            data = request.form['choosed']
-            responses.append(data)
-            print(ind)
-            print(len(responses))
-            ind += 1
-            return redirect('/question/' + str(ind))
+def display_and_handle_answers():
+    if session['start']:
+        if session['ind'] == 0:
+            if request.form.get('choosed'):
+                session['ind'] += 1
+                session['responses'].append(request.form['choosed'])
+                print(session['ind'])
+                print(session['responses'])
+                return redirect('/question/' + str(session['ind']))
+            else:
+                flash('Select an option')
+                return redirect('/question/' + str(session['ind']))
+        elif session['ind'] < satlen:
+            if request.form.get('choosed'):
+                session['ind'] += 1
+                session['responses'].append(request.form['choosed'])
+                print(session['ind'])
+                print(session['responses'])
+                return redirect('/question/' + str(session['ind']))
+            else:
+                flash('Select an option')
+                return redirect('/question/' + str(session['ind']))
+        elif len(session['responses']) == satlen:
+            return render_template('answer.html')
     else:
-        data = request.form['choosed']
-        responses.append(data)
-        print(ind)
-        print(len(responses))
-        return render_template('answer.html')
+        flash('Please start the survey')
+        return redirect('/home')
